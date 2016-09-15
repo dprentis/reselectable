f1c2841749c091af0c8420fa23fcaa4083672ff7
Daniel Prentis <daniel@salsitasoft.com>
2016-09-15 20:45:12 +0200

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,89 @@
+import { List, Map, is, Iterable } from 'immutable';
+import { isEqual, isArray, isObject, map, reduce, every } from 'lodash';
+import { createSelector } from 'reselect';
+
+// selector wrapper to provide the transform function
+// with the previous output in an extra last input parameter
+// the reset method allows to clear the previous value
+// in special use cases eg. unit tests
+export const createMemoSelector = (...args) => {
+    let memo = null;
+    const transFn = args.pop();
+    const transMemoFn = (...newArgs) => (memo = transFn(...newArgs, memo));
+
+    const selector = createSelector(...args.concat(transMemoFn));
+    selector.reset = () => { memo = null; };
+
+    return selector;
+};
+
+const processImmutableWith = (fn, prev) => (item, key) => fn(prev.get(key), item);
+const processPlainWith = (fn, prev) => (item, key) => fn(prev[key], item);
+
+// returns true if and only if all properties of next and its sub-properties
+// are either equal to (===) corresponding properties of prev
+// or are different (neither 'is' or 'isEqual' is true)
+const doTakeNext = (prev, next) => {
+    if (prev === next || !isObject(prev) || !isObject(next)) {
+        return true;
+    }
+
+    if (Iterable.isIterable(prev) && Iterable.isIterable(next)) {
+        return !is(prev, next) ? next.every(processImmutableWith(doTakeNext, prev)) : false;
+    }
+
+    if (!Iterable.isIterable(prev) && !Iterable.isIterable(next)) {
+        return !isEqual(prev, next) ? every(next, processPlainWith(doTakeNext, prev)) : false;
+    }
+
+    return true;
+};
+
+const buildResult = (prev, next) => {
+    if (doTakeNext(prev, next)) {
+        return next;
+    }
+
+    if (Iterable.isIterable(prev) && Iterable.isIterable(next)) {
+        if (is(prev, next)) {
+            return prev;
+        }
+
+        if (List.isList(next) || Map.isMap(next)) {
+            return next.map(processImmutableWith(buildResult, prev));
+        }
+
+        // must convert Record to Map, to be able to map properties
+        // https://github.com/facebook/immutable-js/issues/268
+        // https://github.com/facebook/immutable-js/issues/410
+        return (next.toMap()).map(processImmutableWith(buildResult, prev));
+    }
+
+    if (!Iterable.isIterable(prev) && !Iterable.isIterable(next)) {
+        if (isEqual(prev, next)) {
+            return prev;
+        }
+
+        if (isArray(prev) && isArray(next)) {
+            return map(next, processPlainWith(buildResult, prev));
+        }
+
+        if (isObject(prev) && isObject(next)) {
+            return reduce(next, (memo, item, key) => ({
+                ...memo,
+                [key]: processPlainWith(buildResult, prev)(item, key)
+            }), {});
+        }
+    }
+
+    return next;
+};
+
+// selector wrapper, which normalizes the transform function's output
+// to minimise reference changes between subsequent outputs
+export const createDiffSelector = (...args) => {
+    const transFn = args.pop();
+    const transDiffFn = (...newArgs) => buildResult(newArgs.pop(), transFn(...newArgs));
+
+    return createMemoSelector(...args.concat(transDiffFn));
+};
